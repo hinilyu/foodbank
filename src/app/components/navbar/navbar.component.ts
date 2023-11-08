2241612aee4d7afb7b08e0fd63c240378e165822
hinilyu <hinilyu@gmail.com>
2023-11-07 20:56:18 -0500

    Home UI updated

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -3,8 +3,10 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.css']
+  styleUrls: ['./navbar.component.css'],
 })
 export class NavbarComponent {
-
+  normal = `fa-solid fa-bars`;
+  hover = 'fa-solid fa-bars fa-beat icon-hover';
+  menuButton: string = this.normal;
 }
