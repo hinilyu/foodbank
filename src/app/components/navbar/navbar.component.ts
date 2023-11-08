f0bc305020228ad4d55e1cf6882f9f1a04daf8bc
hinilyu <hinilyu@gmail.com>
2023-11-08 00:36:46 -0500

    UI big update

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,12 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.css'],
 })
-export class NavbarComponent {
+export class NavbarComponent implements OnInit {
   normal = `fa-solid fa-bars`;
   hover = 'fa-solid fa-bars fa-beat icon-hover';
   menuButton: string = this.normal;
+  isLoggedIn: boolean = localStorage.getItem('loggedIn') == 'true' || false;
+
+  ngOnInit(): void {
+    console.log(this.isLoggedIn);
+  }
+
+  logout() {
+    localStorage.setItem('loggedIn', 'false');
+    this.isLoggedIn = false;
+  }
 }
